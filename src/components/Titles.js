ba0a455a5d94ac99844092de3135e868969aadb5
Crystal Shir Navarro <crystal.shir@gmail.com>
2019-09-21 15:55:45 -0700

    initial commit

---
+++ src/components/Titles.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+class Titles extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>Whether Weather</h1>
+        <p>Don't know whether if it's going to Hot or cold? Rain or shine? Wind or humidity? Check the weather here so you can plan your day out right 😉</p>
+      </div>
+    )
+  }
+}
+
+export default Titles;
+
