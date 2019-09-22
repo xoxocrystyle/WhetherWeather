e76700480927e3c467a0ecaaca01133cbfa0b1a5
Crystal Shir Navarro <crystal.shir@gmail.com>
2019-09-22 16:10:51 -0700

    created state to render api

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,11 @@ class Weather extends React.Component{
     render(){
         return (
             <div>
-            Weather component
+            {this.props.city && this.props.country &&  <p>Location: {this.props.city}, {this.props.country}</p>}
+            {this.props.temperature && <p>Temperature: {this.props.temperature}F</p>}
+            {this.props.humidity &&   <p>Humidity: {this.props.humidity}%</p>}
+            {this.props.description &&  <p>Condititons: {this.props.description}</p>}
+            {this.props.error && <p>{this.props.error}</p>}
             </div>
         )
     }
